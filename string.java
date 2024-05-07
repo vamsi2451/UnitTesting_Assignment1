e591bba3da1f9532672dd31291e1b8dac203cc43
vamsi2451 <94015986+vamsi2451@users.noreply.github.com>
2024-05-07 14:51:18 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void toUppercase(char[] str) {
+        for (int i = 0; i < str.length; ++i) {
+            if (Character.isLetter(str[i]) && Character.isLowerCase(str[i])) {
+                str[i] = Character.toUpperCase(str[i]);
+            }
+        }
+        System.out.println(str);
+    }
+    public static void main(String[] args) {
+        char[] str = "hello world".toCharArray();
+        toUppercase(str);
+    }
+}
