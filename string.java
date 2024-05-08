455dff71e90eca74fa2ba45169a601f88bcff51a
vamsi2451 <94015986+vamsi2451@users.noreply.github.com>
2024-05-08 14:33:10 +0530

    Update string.java

--- string.java
+++ string.java
@@ -1,14 +1,58 @@
 public class Main {
-    public static void toUppercase(char[] str) {
-        for (int i = 0; i < str.length; ++i) {
-            if (Character.isLetter(str[i]) && Character.isLowerCase(str[i])) {
-                str[i] = Character.toUpperCase(str[i]);
-            }
+    public static String toUpper(String str){
+    String s = "";
+    for(int i=0 ; i<str.length();i++){
+        if(Character.isLowerCase(str.charAt(i))){
+            s += (char)(str.charAt(i)-32);
         }
-        System.out.println(str);
+        else{
+            s += str.charAt(i)+"";
+        }
     }
-    public static void main(String[] args) {
-        char[] str = "vamsi".toCharArray();
-        toUppercase(str);
+    return s;
+}
+
+    public static boolean test_case1(String str){
+        String compare="VAMSI";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case2(String str){
+        String compare="VAMSI";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case3(String str){
+        String compare="VAMSI";
+        return toUpper(str).equals(compare);
+    }
+     public static boolean test_case4(String str){
+        String compare="VA@MSI";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case5(String str){
+        String compare="";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case6(String str){
+        String compare="1234";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case7(String str){
+        String compare="@#$";
+        return toUpper(str).equals(compare);
+    }
+    public static boolean test_case8(String str){
+        String compare="VA2451MSI";
+        return toUpper(str).equals(compare);
+    }
+public static void main(String[] args) {
+        System.out.println(test_case1("vamsi"));
+        System.out.println(test_case2("VAMSI"));
+        System.out.println(test_case3("vAmSI"));
+        System.out.println(test_case4("va@msi"));
+        System.out.println(test_case5(""));
+        System.out.println(test_case6("1234"));
+        System.out.println(test_case7("@#$"));
+        System.out.println(test_case8("va2451msi"));
+        
     }
 }
